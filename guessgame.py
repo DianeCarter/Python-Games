2e077106b62abb5693f33c615280e79ca35c59cf
Diane <dianecarter632@gmail.com>
2026-06-15 17:53:50 +0700

    updated guessgame.py

--- guessgame.py
+++ guessgame.py
@@ -1,17 +1,26 @@
 import random
 thenumber=random.randint(1,99)
+print("""Some rules:
+If 'You are quite near', it means you have reached a range that the number plus or minus 5
+If 'It is too low' or 'It is too high', they mean you have reached out of the range where the number plus or minus 5""")
 print("Are you ready to take a game named Guess the Number?")
-prompt="What's the number (2 digits) in your mind? "
+prompt="What's the number in your mind? "
 while True:
-    players_guess=int(input(prompt))
+    try:
+        players_guess=int(input(prompt))
+    except ValueError:
+        print("Please enter a positive integer number with 2 digits!")
+        continue
     if players_guess==thenumber:
-        print("You're correct!")
+        print("You are correct!")
         break
-    elif 0<players_guess<thenumber-10:
-        print("It's too low")
-    elif thenumber-10<=players_guess<=thenumber+10:
-        print("You're quite near!")
-    #elif 29<=players_guess<=35:
-        #print("You're quite near!")
-    elif players_guess>thenumber+10:
-        print("It's too high")
+    elif 0<players_guess<thenumber-5:
+        print("It is too low")
+    elif thenumber-5<=players_guess<=thenumber+5:
+        print("You are quite near!")
+    elif 100>players_guess>thenumber+5:
+        print("It is too high")
+    elif players_guess>=100:
+        print("Please enter a number from 1 to 99")
+    elif players_guess<=0:
+        print("Please enter a number from 1 to 99")
