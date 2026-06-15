4b1141210ae1f177f900010503f78d673df5465f
Diane <dianecarter632@gmail.com>
2026-06-15 17:14:55 +0700

    new changes for guessing game

--- guessgame.py
+++ guessgame.py
@@ -1,16 +1,17 @@
-thenumber=28
+import random
+thenumber=random.randint(1,99)
 print("Are you ready to take a game named Guess the Number?")
 prompt="What's the number (2 digits) in your mind? "
 while True:
-	  players_guess=int(input(prompt))
-  	if players_guess==thenumber:
-		    print("You're correct!")
-	    	break
-  	elif 0<players_guess<20:
-	    	print("It's too low")
-	  elif 20<=players_guess<=27:
-	    	print("You're quite near!")
-  	elif 29<=players_guess<=35:
-		    print("You're quite near!")
-	  else:
-	    	print("It's too high")
+    players_guess=int(input(prompt))
+    if players_guess==thenumber:
+        print("You're correct!")
+        break
+    elif 0<players_guess<thenumber-10:
+        print("It's too low")
+    elif thenumber-10<=players_guess<=thenumber+10:
+        print("You're quite near!")
+    #elif 29<=players_guess<=35:
+        #print("You're quite near!")
+    elif players_guess>thenumber+10:
+        print("It's too high")
